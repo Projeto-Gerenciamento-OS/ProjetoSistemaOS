471d50e6b4265f3126c78b48f8d4e0eee03969c9
Gustavo <gustavo.estudo017@gmailcom>
2024-09-24 19:09:45 -0300

    dashbrde pronto e sem sal kkk

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,24 +6,39 @@
 
 
     <div class="BodyLayout">
-        <div class="dashCard">
-            <div class='dashTexto'>
-                <h1>oi bb</h1>    
-            </div>
-        </div>
+        
+        <a class='dashCard' href="{{route('colaborador.index') }}">
+            <i class="fa-solid fa-people-group"></i>
+            
+            <span class="dashTexto">
+                Colaborador
+            </span>
+        </a>
 
-        <div class="dashCard">
-            <div class='dashTexto'>
-                <h1>oi bb</h1>    
-            </div>
-        </div>
+        <a class='dashCard' href="{{route('os.index') }}">
+            <i class="fa-solid fa-box-open"></i>
+            
+            <span class="dashTexto">
+                Ordem de Serviço
+            </span>
+        </a>
 
-        <div class="dashCard">
-            <div class='dashTexto'>
-                <h1>oi bb</h1>    
-            </div>
-        </div>
+        <a class='dashCard' href="{{route('fullcalender.index') }}">
+            <i class="fa-solid fa-calendar"></i>
+            
+            <span class="dashTexto">
+                Agenda
+            </span>
+        </a>
 
+        <a class='dashCard' href="{{route('login.delete') }}">
+                <i class="fa-solid fa-arrow-right-from-bracket "></i>
+        
+            <span class="dashTexto">
+                Sair
+            </span>
+        </a>
     </div>
 </div>
+
 @endsection
